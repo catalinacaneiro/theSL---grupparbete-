6415a120c81d1cc360b4c262c40de3b46b9d1a5e
“Tom <tom.kihlborg@gmail.com>
2025-11-06 15:04:45 +0100

    styling! and added expected time display

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,28 @@ import type { Departure } from './models/Departure';
 
 const p = fetch("https://transport.integration.sl.se/v1/sites/1304/departures");
 
-const app = document.getElementById("app"); 
+const app = document.getElementById("app");
 
-p.then((response: Response) => response.json()).then((data:SlResponse) => {
-  
-  data.departures.forEach((departure: Departure) => { 
+p.then((response: Response) => response.json()).then((data: SlResponse) => {
+
+  data.departures.forEach((departure: Departure) => {
     const containerDestination = document.createElement("div");
     const titelDestination = document.createElement("h2");
-    
+    const expectedTime = document.createElement("p");
+
     console.log(departure.destination);
 
     containerDestination.className = "departure";
-    titelDestination.innerHTML = departure.destination; 
+    titelDestination.innerHTML = departure.destination;
+    expectedTime.innerHTML = departure.display;
 
     app?.appendChild(containerDestination);
     containerDestination.appendChild(titelDestination);
+    containerDestination.appendChild(expectedTime);
+
+
 
   });
-
 
 
 
